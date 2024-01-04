59728b20a81069dd5bb0e2404171d42250b37bf8
alinadolbilova <genin.10@mail.ru>
2024-01-04 14:04:12 +0300

    add_FUNCTION

--- homework-1/src/channel.py
+++ homework-1/src/channel.py
@@ -1,3 +1,10 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('API_YouTube')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +12,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.dict_to_print = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.dict_to_print, indent=2, ensure_ascii=False))
