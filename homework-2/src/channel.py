602fd8dd7e00450cde75d18356e054cf09c2c062
alinadolbilova <genin.10@mail.ru>
2024-01-06 01:03:55 +0300

    add_FUNCTION_HW2

--- homework-2/src/channel.py
+++ homework-2/src/channel.py
@@ -5,11 +5,11 @@ from googleapiclient.discovery import build
 
 
 class Channel:
-
     """Класс для ютуб-канала"""
 
 
 api_key: str = os.getenv('API_YouTube')
+
 
 def __init__(self, channel_id: str) -> None:
     """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -24,10 +24,11 @@ def __init__(self, channel_id: str) -> None:
     self.viewCount = dict_youtube["items"][0]["statistics"]["viewCount"]
     self.url = dict_youtube["items"][0]["snippet"]["thumbnails"]["default"]["url"]
 
-@classmethod
-def get_service(cls):
+
+def get_service(self):
     youtube = build('youtube', 'v3', developerKey=api_key)
     return youtube
+
 
 def to_json(self, file_name):
     data = {
@@ -42,9 +43,11 @@ def to_json(self, file_name):
     with open(file_name, 'w', encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+
 @property
 def channel_id(self):
     return self.__channel_id
+
 
 @channel_id.setter
 def channel_id(self, value):
