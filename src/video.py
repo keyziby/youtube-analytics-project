ba97004e43f388348af09379c68a9c1b8670a7da
alinadolbilova <genin.10@mail.ru>
2024-01-22 16:19:33 +0300

    add_function_video

---
+++ src/video.py
@@ -0,0 +1,30 @@
+from googleapiclient.discovery import build
+import os
+
+
+class Video:
+    api_key: str = os.getenv('YT_API_KEY')
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                               id=self.video_id).execute()
+        self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/watch?v={self.video_response['items'][0]['id']}"
+        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
