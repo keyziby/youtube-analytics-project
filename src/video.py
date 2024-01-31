47ad5443d3475a68494bebfdaf7271e391a5ffe2
alinadolbilova <genin.10@mail.ru>
2024-01-31 14:03:40 +0300

    add_try_exc

--- src/video.py
+++ src/video.py
@@ -6,14 +6,20 @@ class Video:
     api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                               id=self.video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_response['items'][0]['id']}"
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.video_id = video_id
+            self.video_response = self.get_service().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',id=self.video_id).execute()
+            self.video_title: str = self.video_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_response['items'][0]['id']}'"
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.video_id = video_id
+            self.video_title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.video_title}'
